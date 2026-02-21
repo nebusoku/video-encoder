8271e0b2aa735617c9e7c167b4c64eb04426e5cb
Jonathan F <nebusoku@gmail.com>
2026-02-21 01:47:32 -0500

    Print script path/version to detect stale copies on host

---
+++ scripts/Start-Movies.ps1
@@ -0,0 +1,12 @@
+[CmdletBinding()]
+param(
+    [Parameter(Mandatory)][string]$RootPath,
+    [switch]$EnableFileBotRename,
+    [switch]$EnsureDependencies,
+    [switch]$RefreshDependencies,
+    [switch]$ProbeHardwareOnly,
+    [switch]$RefreshHardwareCache
+)
+
+$entry = Join-Path (Split-Path -Parent $PSScriptRoot) "video-convert.ps1"
+& $entry -RootPath $RootPath -Mode 2 -EnableFileBotRename:$EnableFileBotRename -EnsureDependencies:$EnsureDependencies -RefreshDependencies:$RefreshDependencies -ProbeHardwareOnly:$ProbeHardwareOnly -RefreshHardwareCache:$RefreshHardwareCache
